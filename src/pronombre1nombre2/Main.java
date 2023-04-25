e81355c02e58ee2076893659611e414b522319e9
Adrián Azaola Lavin <adrianazaolalavin@MacbookPro.local>
2023-04-25 23:05:49 +0200

    Tercer cambio

--- src/pronombre1nombre2/Main.java
+++ src/pronombre1nombre2/Main.java
@@ -3,6 +3,7 @@ package pronombre1nombre2;
 public class Main {
 	public static void main(String[] args){
 		System.out.println("Hola Mundo y Marce!");
-		System.out.println("Segundo Cambioo");
+		System.out.println("Segundo Cambio");
+		System.out.println("Tercer Cambioo");
 	}
 }
