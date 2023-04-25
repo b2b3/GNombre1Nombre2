aca70a98f0fb08d4ca755956507bf24d1cd92b6a
Adrián Azaola Lavin <adrianazaolalavin@MacbookPro.local>
2023-04-25 22:49:31 +0200

    Subir a repositorio github

--- src/pronombre1nombre2/Main.java
+++ src/pronombre1nombre2/Main.java
@@ -3,6 +3,6 @@ package pronombre1nombre2;
 public class Main {
 	public static void main(String[] args){
 		System.out.println("Hola Mundo y Marce!");
-		System.out.println("Segundo Cambio!");
+		System.out.println("Segundo Cambioo");
 	}
 }
